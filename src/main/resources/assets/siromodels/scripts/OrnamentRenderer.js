afc1d52a5cd763580e27cb6b16b6a59a4619d6f7
Siro_256 <siro@siro256.dev>
2023-06-19 00:47:30 +0900

    Create scripts

---
+++ src/main/resources/assets/siromodels/scripts/OrnamentRenderer.js
@@ -0,0 +1,7 @@
+var renderClass = "jp.ngt.rtm.render.OrnamentPartsRenderer";
+
+importPackage(Packages.dev.siro256.rtmpack.siromodels.router);
+
+function init(modelSet, modelObject) {
+    RendererRouter.init(renderer, modelObject);
+}
---
+++ src/main/resources/assets/siromodels/scripts/Server.js
@@ -0,0 +1,5 @@
+importPackage(Packages.dev.siro256.rtmpack.siromodels.router);
+
+function onUpdate(entity, scriptExecutor) {
+    ServerRouter.onUpdate(entity);
+}
