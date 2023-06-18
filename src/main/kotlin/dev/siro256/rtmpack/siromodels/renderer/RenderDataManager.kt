1d2b1bfd169979cf42ab6878504ec4339b4734b5
Siro_256 <siro@siro256.dev>
2023-06-19 00:44:08 +0900

    Create render data manager

---
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/RenderDataManager.kt
@@ -0,0 +1,7 @@
+package dev.siro256.rtmpack.siromodels.renderer
+
+import dev.siro256.rtmpack.siromodels.model.ModelRegistry
+
+object RenderDataManager {
+    val models = HashMap<String, ModelRegistry>()
+}
