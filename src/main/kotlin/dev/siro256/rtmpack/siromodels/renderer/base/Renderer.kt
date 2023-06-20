6409d0a020f263d60f2158c27a14010781538841
Siro_256 <siro@siro256.dev>
2023-06-20 19:03:37 +0900

    Create Matrix4fStack.stack for Renderer

--- src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/Renderer.kt
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/Renderer.kt
@@ -1,7 +1,15 @@
 package dev.siro256.rtmpack.siromodels.renderer.base
 
+import org.joml.Matrix4fStack
+
 interface Renderer {
     var currentTexture: Int
 
     fun Float.toRadians() = Math.toRadians(toDouble()).toFloat()
+
+    fun Matrix4fStack.stack(block: () -> Unit) {
+        pushMatrix()
+        block.invoke()
+        popMatrix()
+    }
 }
