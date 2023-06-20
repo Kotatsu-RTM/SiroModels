bdc37aebe72600132d441e6a71f4b158b880a268
Siro_256 <siro@siro256.dev>
2023-06-20 18:41:58 +0900

    Using shader with LightRenderer

--- src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/LightRenderer.kt
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/LightRenderer.kt
@@ -1,69 +1,95 @@
 package dev.siro256.rtmpack.siromodels.renderer
 
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader.Builder.Companion.bindVBO
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader.Builder.Companion.render
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader.Builder.Companion.setLightMapCoords
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader.Builder.Companion.setMaterial
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader.Builder.Companion.setModelView
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader.Builder.Companion.setTexture
+import com.github.kotatsu_rtm.kotatsulib.api.shader.TexturedShader.Builder.Companion.useModel
 import dev.siro256.rtmpack.siromodels.model.ornament.LightModel
-import jp.ngt.ngtlib.renderer.GLHelper
+import dev.siro256.rtmpack.siromodels.renderer.base.CustomOrnamentPartsRenderer
 import jp.ngt.rtm.block.tileentity.TileEntityFluorescent
-import jp.ngt.rtm.render.OrnamentPartsRenderer
 import jp.ngt.rtm.render.RenderPass
 import net.minecraft.tileentity.TileEntity
-import org.lwjgl.opengl.GL11
+import org.joml.Matrix4f
+import org.joml.Vector2f
 
-class LightRenderer : OrnamentPartsRenderer() {
+class LightRenderer : CustomOrnamentPartsRenderer() {
     private val model by lazy { RenderDataManager.models[modelName] as LightModel }
 
-    override fun render(tileEntity: TileEntity?, pass: RenderPass, tickProgression: Float) {
+    override fun render(
+        tileEntity: TileEntity?,
+        pass: RenderPass,
+        tickProgression: Float,
+        modelViewMatrix: Matrix4f,
+        projectionMatrix: Matrix4f,
+        lightMapCoords: Vector2f,
+    ) {
         if (pass != RenderPass.NORMAL) return
-        val modelHeight = 0.15F
+        if (tileEntity !is TileEntityFluorescent?) return
 
-        GL11.glPushMatrix()
-
-        if (tileEntity is TileEntityFluorescent) {
+        if (tileEntity != null) {
             when (tileEntity.dir.toInt()) {
-                0 -> GL11.glTranslatef(0.0F, 0.5F - modelHeight, 0.0F)
+                0 -> {
+                    modelViewMatrix.translate(0.0F, 0.5F - MODEL_HEIGHT, 0.0F)
+                }
+
                 1 -> {
-                    GL11.glTranslatef(0.0F, 0.0F, 0.5F - modelHeight)
-                    GL11.glRotatef(90.0F, 1.0F, 0.0F, 0.0F)
+                    modelViewMatrix.translate(0.0F, 0.0F, 0.5F - MODEL_HEIGHT)
+                    modelViewMatrix.rotateX(90.0F.toRadians())
                 }
 
                 2 -> {
-                    GL11.glTranslatef(0.0F, -0.5F + modelHeight, 0.0F)
-                    GL11.glRotatef(180.0F, 1.0F, 0.0F, 0.0F)
+                    modelViewMatrix.translate(0.0F, -0.5F + MODEL_HEIGHT, 0.0F)
+                    modelViewMatrix.rotateX(180.0F.toRadians())
                 }
 
                 3 -> {
-                    GL11.glTranslatef(0.0F, 0.0F, -0.5F + modelHeight)
-                    GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F)
+                    modelViewMatrix.translate(0.0F, 0.0F, -0.5F + MODEL_HEIGHT)
+                    modelViewMatrix.rotateX((-90.0F).toRadians())
                 }
 
                 4 -> {
-                    GL11.glTranslatef(0.0F, 0.5F - modelHeight, 0.0F)
-                    GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F)
+                    modelViewMatrix.translate(0.0F, 0.5F - MODEL_HEIGHT, 0.0F)
+                    modelViewMatrix.rotateY(90.0F.toRadians())
                 }
 
                 5 -> {
-                    GL11.glTranslatef(0.5F - modelHeight, 0.0F, 0.0F)
-                    GL11.glRotatef(-90.0F, 0.0F, 1.0F, 0.0F)
-                    GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F)
+                    modelViewMatrix.translate(0.5F - MODEL_HEIGHT, 0.0F, 0.0F)
+                    modelViewMatrix.rotateY((-90.0F).toRadians())
+                    modelViewMatrix.rotateX((-90.0F).toRadians())
                 }
 
                 6 -> {
-                    GL11.glTranslatef(0.0F, -0.5F + modelHeight, 0.0F)
-                    GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F)
-                    GL11.glRotatef(-180.0F, 1.0F, 0.0F, 0.0F)
+                    modelViewMatrix.translate(0.0F, -0.5F + MODEL_HEIGHT, 0.0F)
+                    modelViewMatrix.rotateY(90.0F.toRadians())
+                    modelViewMatrix.rotateX((-180.0F).toRadians())
                 }
 
                 7 -> {
-                    GL11.glTranslatef(-0.5F + modelHeight, 0.0F, 0.0F)
-                    GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F)
-                    GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F)
+                    modelViewMatrix.translate(-0.5F + MODEL_HEIGHT, 0.0F, 0.0F)
+                    modelViewMatrix.rotateY(90.0F.toRadians())
+                    modelViewMatrix.rotateX((-90.0F).toRadians())
                 }
             }
         }
 
-        GLHelper.setBrightness(Int.MAX_VALUE)
-        model.body.render(this)
-        model.light.render(this)
+        TexturedShader
+            .updateProjection(projectionMatrix)
+            .setMaterial(currentMatId)
+            .setTexture(currentTexture)
+            .bindVBO(model.vbo)
+            .setLightMapCoords(lightMapCoords)
+            .setModelView(modelViewMatrix)
+            .useModel(model.body)
+            .render()
+            .useModel(model.light)
+            .render()
+    }
 
-        GL11.glPopMatrix()
+    companion object {
+        private const val MODEL_HEIGHT = 0.15F
     }
 }
