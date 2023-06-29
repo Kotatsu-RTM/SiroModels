399fe0fd897af3fd53d4dce39b9e9df08bb21fe8
Siro_256 <siro@siro256.dev>
2023-06-29 18:27:34 +0900

    Update base renderer classes to dealing with KotatsuLib update

--- src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/CustomMachinePartsRenderer.kt
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/CustomMachinePartsRenderer.kt
@@ -2,6 +2,7 @@ package dev.siro256.rtmpack.siromodels.renderer.base
 
 import dev.siro256.rtmpack.siromodels.CustomModelObject
 import dev.siro256.rtmpack.siromodels.deepCopy
+import jp.ngt.rtm.block.tileentity.TileEntityMachineBase
 import jp.ngt.rtm.render.MachinePartsRenderer
 import jp.ngt.rtm.render.ModelObject
 import jp.ngt.rtm.render.RenderPass
@@ -12,6 +13,7 @@ import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.math.BlockPos
 import org.joml.Matrix4f
 import org.joml.Vector2f
+import org.joml.Vector3f
 import org.lwjgl.opengl.GL11
 import kotlin.properties.Delegates
 
@@ -19,7 +21,7 @@ abstract class CustomMachinePartsRenderer : MachinePartsRenderer(), Renderer {
     private var previousFrameIndex = Int.MIN_VALUE
     private var previousTileEntityPos = BlockPos(100_000_000, 100_000_000, 100_000_000)
 
-    private var modelViewMatrix by Delegates.notNull<Matrix4f>()
+    private var modelViewMatrix by Delegates.notNull<ModelViewMatrix>()
     private var projectionMatrix by Delegates.notNull<Matrix4f>()
     private val matrixBuffer = GLAllocation.createDirectFloatBuffer(16)
 
@@ -27,6 +29,9 @@ abstract class CustomMachinePartsRenderer : MachinePartsRenderer(), Renderer {
 
     @Suppress("DuplicatedCode")
     final override fun render(tileEntity: TileEntity?, pass: RenderPass, tickProgression: Float) {
+        if (tileEntity !is TileEntityMachineBase?)
+            throw IllegalArgumentException("An unexpected type \"${tileEntity?.javaClass}\" was passed")
+
         if (currentTexture == -1) {
             modelSet.modelObj = modelSet.modelObj.deepCopy(ModelObject::class.java, CustomModelObject())
             currentTexture = 0
@@ -43,7 +48,7 @@ abstract class CustomMachinePartsRenderer : MachinePartsRenderer(), Renderer {
                     rewind()
                     GL11.glGetFloat(GL11.GL_MODELVIEW_MATRIX, this)
                     rewind()
-                }.let { Matrix4f(it) }
+                }.let { ModelViewMatrix(Matrix4f(it)) }
 
             projectionMatrix =
                 matrixBuffer.apply {
@@ -53,6 +58,13 @@ abstract class CustomMachinePartsRenderer : MachinePartsRenderer(), Renderer {
                 }.let { Matrix4f(it) }
         }
 
+        val modelMatrix = Matrix4f()
+        tileEntity?.pos?.let { modelMatrix.translate(it.x.toFloat(), it.y.toFloat(), it.z.toFloat()) }
+        tileEntity?.resourceState?.resourceSet?.config?.offset?.let { modelMatrix.translate(Vector3f(it)) }
+
+        //Cancel previous operations
+        modelViewMatrix.matrix.mul(Matrix4f(modelMatrix).invert())
+
         val lightMapCoords =
             if (tileEntity == null) {
                 Vector2f(248.0F / 256.0F, 248.0F / 256.0F)
@@ -60,14 +72,15 @@ abstract class CustomMachinePartsRenderer : MachinePartsRenderer(), Renderer {
                 Vector2f((OpenGlHelper.lastBrightnessX + 8.0F) / 256.0F, (OpenGlHelper.lastBrightnessY + 8.0F) / 256.0F)
             }
 
-        render(tileEntity, pass, tickProgression, modelViewMatrix, projectionMatrix, lightMapCoords)
+        render(tileEntity, pass, tickProgression, modelMatrix, modelViewMatrix, projectionMatrix, lightMapCoords)
     }
 
     abstract fun render(
         tileEntity: TileEntity?,
         pass: RenderPass,
         tickProgression: Float,
-        modelViewMatrix: Matrix4f,
+        modelMatrix: Matrix4f,
+        modelViewMatrix: ModelViewMatrix,
         projectionMatrix: Matrix4f,
         lightMapCoords: Vector2f,
     )
