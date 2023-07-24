cacbf94e5aff393ecd09d1868bb4ca32c42f4d36
Siro_256 <siro@siro256.dev>
2023-07-24 09:12:41 +0900

    Create tileentity renderer base

---
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/MachineRenderer.kt
@@ -0,0 +1,40 @@
+package dev.siro256.rtmpack.siromodels.renderer.base
+
+import dev.siro256.rtmpack.siromodels.CustomModelObject
+import dev.siro256.rtmpack.siromodels.deepCopy
+import jp.ngt.rtm.block.tileentity.TileEntityMachineBase
+import jp.ngt.rtm.render.ModelObject
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer
+import org.joml.Matrix4f
+import org.joml.Vector2f
+import org.joml.Vector3f
+
+abstract class MachineRenderer<T : TileEntityMachineBase> : TileEntitySpecialRenderer<T>(), Renderer {
+    final override var currentTexture = -1
+
+    final override fun render(
+        tileEntity: T,
+        x: Double, y: Double, z: Double,
+        tickProgression: Float,
+        destroyStage: Int,
+        alpha: Float
+    ) {
+        if (currentTexture == -1) {
+            tileEntity.resourceState?.resourceSet?.modelObj =
+                tileEntity.resourceState?.resourceSet?.deepCopy(ModelObject::class.java, CustomModelObject())
+            currentTexture = 0
+        }
+
+        val modelMatrix = Matrix4f().translate(x.toFloat(), y.toFloat(), z.toFloat())
+        tileEntity.resourceState?.resourceSet?.config?.offset?.let { modelMatrix.translate(Vector3f(it)) }
+
+        render(tileEntity, tickProgression, modelMatrix, tileEntity.getLightMapCoordinate())
+    }
+
+    abstract fun render(
+        tileEntity: T?,
+        tickProgression: Float,
+        modelMatrix: Matrix4f,
+        lightMapCoords: Vector2f,
+    )
+}
--- src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/Renderer.kt
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/Renderer.kt
@@ -1,6 +1,8 @@
 package dev.siro256.rtmpack.siromodels.renderer.base
 
+import net.minecraft.tileentity.TileEntity
 import org.joml.Matrix4fStack
+import org.joml.Vector2f
 
 interface Renderer {
     var currentTexture: Int
@@ -11,5 +13,13 @@ interface Renderer {
         pushMatrix()
         block.invoke()
         popMatrix()
+    }
+
+    fun TileEntity.getLightMapCoordinate(): Vector2f {
+        val brightness = world.getCombinedLight(pos, world.getLight(pos))
+        return Vector2f(
+            (brightness % 65536).toFloat() / 256.0F * 0.9375F + 0.03125F,
+            (brightness / 65536).toFloat() / 256.0F * 0.9375F + 0.03125F
+        )
     }
 }
