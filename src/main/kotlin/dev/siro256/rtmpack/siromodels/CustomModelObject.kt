906eaccbfffad34654dde4aa435a3c3c866e8977
Siro_256 <siro@siro256.dev>
2023-07-26 10:55:41 +0900

    Skip render if the renderer is not my renderer

--- src/main/kotlin/dev/siro256/rtmpack/siromodels/CustomModelObject.kt
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/CustomModelObject.kt
@@ -1,10 +1,12 @@
 package dev.siro256.rtmpack.siromodels
 
+import dev.siro256.rtmpack.siromodels.renderer.base.Renderer
 import jp.ngt.rtm.render.ModelObject
 import jp.ngt.rtm.render.RenderPass
 
 class CustomModelObject : ModelObject(null, null) {
     override fun renderWithTexture(entity: Any?, pass: RenderPass, tickProgression: Float) {
+        if (renderer !is Renderer) return
         renderer.render(entity, pass, tickProgression)
     }
 }
