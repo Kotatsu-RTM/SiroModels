d7d6d71337f6f15a79c8e57b163aa7b6ca6b46d2
Siro_256 <siro@siro256.dev>
2023-07-27 01:27:07 +0900

    Move origin to center of block

--- src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/MachineRenderer.kt
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/MachineRenderer.kt
@@ -2,4 +2,9 @@ package dev.siro256.rtmpack.siromodels.renderer.base
 
 import jp.ngt.rtm.block.tileentity.TileEntityMachineBase
 
-abstract class MachineRenderer<T : TileEntityMachineBase> : TileEntityRenderer<T>()
+abstract class MachineRenderer<T : TileEntityMachineBase> : TileEntityRenderer<T>() {
+    override fun modelOffset(tileEntity: T?, modelName: String) =
+        super.modelOffset(tileEntity, modelName).also {
+            if (tileEntity != null) it.translate(0.5F, 0.0F, 0.5F).rotateY(tileEntity.rotation.toRadians())
+        }
+}
