833bba7b7a7f6eccd3159dfe78be43e866f200eb
Siro_256 <siro@siro256.dev>
2023-07-26 10:53:42 +0900

    Implement Renderer for TileEntityRenderer

--- src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/TileEntityRenderer.kt
+++ src/main/kotlin/dev/siro256/rtmpack/siromodels/renderer/base/TileEntityRenderer.kt
@@ -9,30 +9,12 @@ import net.minecraft.client.renderer.texture.SimpleTexture
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.ResourceLocation
 import org.joml.Matrix4f
-import org.joml.Matrix4fStack
 import org.joml.Vector2f
 import org.joml.Vector3f
 
-abstract class TileEntityRenderer<T>
+abstract class TileEntityRenderer<T> : Renderer
         where T : TileEntity,
               T : IResourceSelector<*> {
-
-    fun Float.toRadians() = Math.toRadians(toDouble()).toFloat()
-
-    fun Matrix4fStack.stack(block: () -> Unit) {
-        pushMatrix()
-        block.invoke()
-        popMatrix()
-    }
-
-    fun TileEntity.getLightMapCoordinate(): Vector2f {
-        val brightness = world.getCombinedLight(pos, world.getLight(pos))
-        return Vector2f(
-            (brightness % 65536).toFloat() / 256.0F * 0.9375F + 0.03125F,
-            (brightness / 65536).toFloat() / 256.0F * 0.9375F + 0.03125F
-        )
-    }
-
     fun getTextureId(textureLocation: ResourceLocation): Int {
         val textureManager = Minecraft.getMinecraft().textureManager
         @Suppress("UNNECESSARY_SAFE_CALL")
